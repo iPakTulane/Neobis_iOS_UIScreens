43aeba71e48222b05c44ec03cf56bd985579736c
iPakTulane <ipak.tulane@gmail.com>
2023-11-08 09:25:21 +0500

    Initial commit

---
+++ Neobis_iOS_UIScreens/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_UIScreens
+//
+//  Created by iPak Tulane on 08/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+        
+    }
+
+
+}
+
